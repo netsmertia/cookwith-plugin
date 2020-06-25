527ca7620e9a70a6a4edf1d6d9ac0bb95ddb71f7
Natwar <nets.developer@gmail.com>
2020-06-25 21:33:16 +0530

    complete

--- recipe/models/Settings.php
+++ recipe/models/Settings.php
@@ -4,11 +4,18 @@ use Model;
 
 class Settings extends Model
 {
-    public $implement = ['System.Behaviors.SettingsModel'];
+    public $implement = ['System.Behaviors.SettingsModel','RainLab.Translate.Behaviors.TranslatableModel'];
+    
+    
 
     // A unique code
     public $settingsCode = 'recipe_recipe_settings';
 
     // Reference to field configuration
     public $settingsFields = 'fields.yaml';
+
+
+    protected $dates = ['deleted_at', 'created_at', 'updated_at'];
+
+    public $translatable = ['about'];
 }
