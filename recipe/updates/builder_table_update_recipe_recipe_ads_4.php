2a6cde37af595f17b98bcfec41b1022cfd8e68df
Natwar <nets.developer@gmail.com>
2020-06-30 00:05:42 +0530

    added sapce type ui

---
+++ recipe/updates/builder_table_update_recipe_recipe_ads_4.php
@@ -0,0 +1,27 @@
+<?php namespace recipe\Recipe\Updates;
+
+use Schema;
+use October\Rain\Database\Updates\Migration;
+
+class BuilderTableUpdateRecipeRecipeAds4 extends Migration
+{
+    public function up()
+    {
+        Schema::table('recipe_recipe_ads', function($table)
+        {
+            $table->boolean('is_active')->default(true);
+            $table->dropColumn('ad_type');
+            $table->dropColumn('ad_space_id');
+        });
+    }
+    
+    public function down()
+    {
+        Schema::table('recipe_recipe_ads', function($table)
+        {
+            $table->dropColumn('is_active');
+            $table->string('ad_type', 191);
+            $table->integer('ad_space_id')->default(0);
+        });
+    }
+}
